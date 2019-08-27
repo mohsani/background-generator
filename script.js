2184806bb67c5282b3bb76dbfb2cf35229e74b22
Mohammad <mohsani1997@gmail.com>
2019-08-27 14:55:12 +0430

    adding button

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const css = document.querySelector('h3');
-const [color1, color2] = document.querySelectorAll('.color')
-const body = document.querySelector('#gradient');
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let button = document.querySelector("button");
 
-setGradient = () => {
-    body.style.background = _linearGradient();
-    css.textContent = `${body.style.background};`;
+function getRandomColor() {
+    let length = 6;
+    let chars = '0123456789ABCDEF';
+    let hex = '#';
+    while (length--) hex += chars[(Math.random() * 16) | 0];
+    return hex;
 }
 
-_linearGradient = () => {
-    return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+// css.textContent = `${color1.value}   ${color2.value}`;
+// css.textContent = body.style.background + ";";
+
+let changeBackground = function() {
+    body.style.background = "linear-gradient(to right , " + color1.value + ", " + color2.value + ")";
+    // css.textContent = `${color1.value}   ${color2.value}`;
+    css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener('input', setGradient);
+button.addEventListener("click", function(event) {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
 
-color2.addEventListener('input', setGradient);
+    changeBackground();
+})
+
+changeBackground();
+
+compBodyStyles = window.getComputedStyle(body);
+css.textContent = compBodyStyles.getPropertyValue('background-image') + ";";
+
+// color1.addEventListener("input", changeBackground);
+//
+// color2.addEventListener("input", changeBackground);
